38d60979ff842c5f3a62f9512644957cdea653f4
unknown <michael.larsen91@gmail.com>
2022-10-21 00:09:42 -0700

    Adding code from halloween 2021

---
+++ mqttTest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#
+import time
+import random
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
+broker = '10.0.0.43'
+state_topic = 'home-assistant/smartcam/person'
+delay = 5
+
+# Send a single message to set the mood
+publish.single('home-assistant/smartcam/person', 'true', hostname=broker)
+
+# Send messages in a loop
+'''client = mqtt.Client("ha-client")
+client.connect(broker)
+client.loop_start()
+
+while True:
+    client.publish(state_topic, random.randrange(0, 50, 1))
+    time.sleep(delay)
+'''
---
+++ smartlamp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /opt/intel/openvino_2021/bin/setupvars.sh
+python3 /home/michaellarsen/open_model_zoo-master/demos/object_detection_demo/python/smartlamp.py
